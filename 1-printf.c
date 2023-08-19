d0d90262fc2dd6ac98b5a9974382ce51a02b3616
akinbye <akinbye3@gmail.com>
2023-08-19 12:43:21 -0700

    1-printf.c

--- 1-printf.c
+++ 1-printf.c
@@ -3,16 +3,21 @@
  *print_int - prints integers
  *@my_list: all arguments
  *@buffer: array for print
+ *@flags: calculates flags
+ *@width: get width
+ *@precision: for precision
+ *@size: for size
  *Return: 0
  */
-int print_int(va_list my_list, char buffer[])
+int print_int(va_list my_list, char buffer[],
+		int flags, int width, int precision, int size)
 {
 	int m = BUFF_SIZE - 2;
 	int is_negative = 0;
 	long int n = va_arg(my_list, long int);
 	unsigned long int val;
 
-	n = 0;
+	n = convert_size_number(n, size);
 
 	if (n == 0)
 		buffer[m--] = '0';
@@ -30,21 +35,31 @@ int print_int(va_list my_list, char buffer[])
 		val /= 10;
 	}
 	m++;
-	return (is_negative, m, *buffer);
+	return (write_number(is_negative, m, buffer, flags, width,
+				precision, size));
 }
 /**
  *print_binary - prints i
  *@b_list: arguments
  *@buffer: an array
+ *@flags: flags
+ *@width: width
+ *@precision: precision
+ *@size: size
  *Return: 0
  */
-int print_binary(va_list b_list, char buffer[])
+int print_binary(va_list b_list, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	unsigned int z, b, c, sum;
 	unsigned int a[32];
 	int count;
 
 	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
 	z = va_arg(b_list, unsigned int);
 	b = 2147483648;
--- main.h
+++ main.h
@@ -7,9 +7,14 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
+void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...);
-int print_int(va_list my_list, char buffer[]);
-int print_binary(va_list b_list, char buffer[]);
-
+int print_int(va_list my_list, char buffer[],
+		int flags, int width, int precision, int size);
+int print_binary(va_list b_list, char buffer[],
+		int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+		int flags, int width, int precision, int size);
+long int convert_size_number(long int num, int size);
 
 #endif
