5e575e1331ef4bfe2ddbe7c2d3b11d46d160790f
akinbye <akinbye3@gmail.com>
2023-08-22 07:40:25 -0700

    printf

---
+++ handle.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ *handle_p - prints arguments
+ *@format: formated string
+ *@my_list: argument list
+ *@ind: ind
+ *@buffer: buffer array
+ *@flags: active flags
+ *@width: get width
+ *@precision: get precision
+ *@size: size
+ *Return: integer
+ */
+int handle_p(const char *format, int *ind, va_list my_list,
+		char buffer[], int flags, int width, int precision, int size)
+{
+	int i, len = 0, all_chars = -1;
+	format_t format_types[] = {
+		{'c', print_c}, {'s', print_s}, {'%', print_per},
+		{'i', print_i}, {'d', print_i}, {'b', print_b},
+		{'u', print_u}, {'o', print_o}, {'x', print_x},
+		{'X', print_X}, {'p', print_p}, {'S', print_non},
+		{'r', print_r}, {'R', print_rot}, {'\0', NULL}
+	};
+
+	for (i = 0; format_types[i].format != '\0'; i++)
+	{
+		if (format[*ind] == format_types[i].format)
+			return (format_types[i].fn(my_list, buffer, flags, width,
+						precision, size));
+	}
+	if (format_types[i].format == '\0')
+	{
+		if (format[*ind] == '\0')
+			return (-1);
+		len += write(1, "%%", 1);
+
+		if (format[*ind - 1] == ' ')
+			len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+
+			while (format[*ind] != ' ' && format[*ind] != '%')
+					--(*ind);
+			if (format[*ind] == ' ')
+			--(*ind);
+			return (1);
+		}
+		len += write(1, &format[*ind], 1);
+		return (len);
+	}
+	return (all_chars);
+}
--- main.h
+++ main.h
@@ -5,11 +5,77 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void print_buffer(char buffer[], int *buff_ind);
-void handle_conversion(int value);
-int _ptchar(char c);
-int _printf(const char *format, ...);
-int put_s(char *c);
-int printf_int(const char *format, ...);
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
-#endif
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
+
+struct format
+{
+	char format;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+typedef struct format format_t;
+void print_buff(char buffer[], int *buff_ind);
+int handle_p(const char *format, int *ind, va_list my_list, char buffer[],
+		int flags, int width, int precision, int size);
+
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+int _printf(const char *format, ...);
+int printf_int(const char *format, ...);
+int print_c(va_list my_arg, char buffer[],
+		int flags, int width, int precision, int size);
+int print_s(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_per(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_i(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_b(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_u(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_o(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_x(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_X(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_p(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_non(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_r(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+int print_rot(va_list my_arg, char buffer[],
+	int flags, int width, int precision, int size);
+
+int handle_w(char c, char buffer[], int flags, int width,
+		int precision, int size);
+int write_n(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_p(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+int write_un(int is_negative, int ind,char buffer[],
+		int flags, int width, int precision, int size);
+
+int printable(char);
+int append(char, char[], int);
+int is_digit(char);
+long int convert_s(long int number, int size);
+long int convert_u(unsigned long int number, int size);
+
+#endif /* MAIN_H */
